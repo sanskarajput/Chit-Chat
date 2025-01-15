6f472fdb9c51007968b3e6cf199a974b87addcc5
sanskarajput <sanskarrajput2005@gmail.com>
2025-01-15 09:20:35 +0000

    chat-models added

--- Application/chats/models.py
+++ Application/chats/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Creating models here.
+class Conversation(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversation_s_user_1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversation_s_user_2')   
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Conversation between {self.user1.username} and {self.user2.username}'
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_s_sender')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} in Conversation {self.conversation.id}"
+    
+    # def __str__(self):
+    #     return f'Message from {self.sender.username} to {self.conversation.user1.username} and {self.conversation.user2.username}'
